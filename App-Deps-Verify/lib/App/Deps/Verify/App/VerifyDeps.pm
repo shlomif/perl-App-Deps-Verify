f6217f53b2ab3814c99e55d12ffdb65c462af6d2
Shlomi Fish <shlomif@shlomifish.org>
2018-09-17 15:19:21 +0300

    more.

---
+++ App-Deps-Verify/lib/App/Deps/Verify/App/VerifyDeps.pm
@@ -0,0 +1,10 @@
+package App::Deps::Verify::App::VerifyDeps;
+
+use strict;
+use warnings;
+use autodie;
+
+use App::Cmd::Setup -app;
+
+1;
+__END__
---
+++ App-Deps-Verify/lib/App/Deps/Verify/App/VerifyDeps/Command/verify.pm
@@ -0,0 +1,42 @@
+package App::Deps::Verify::App::VerifyDeps::Command::verify;
+
+use App::Deps::Verify::App::VerifyDeps -command;
+
+use strict;
+use warnings;
+
+use Path::Tiny qw/ path /;
+use App::Deps::Verify ();
+
+sub abstract { "verify the presence of dependencies" }
+
+sub description { return abstract(); }
+
+sub opt_spec
+{
+    return (
+        [ "output|o=s", "path to output file" ],
+        [ "input|i=s",  "the input file" ],
+    );
+}
+
+sub validate_args
+{
+    my ( $self, $opt, $args ) = @_;
+
+    # no args allowed but options!
+    $self->usage_error("No args allowed") if @$args;
+}
+
+sub execute
+{
+    my ( $self, $opt, $args ) = @_;
+
+    App::Deps::Verify->new->verify_deps_in_yamls(
+        +{ filenames => [ $opt->{input}, ] } );
+
+    path( $opt->{output} )->spew_utf8("Success!");
+}
+
+1;
+
